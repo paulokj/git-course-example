dfcf393a9d72cd619497011f3a45e645a7430891
Paul Addai <pauljnraddai@yahoo.com>
2025-04-01 08:32:52 +0100

    WIP: 62514cf Stuff

--- folder/git_tutoria.l.py
+++ folder/git_tutoria.l.py
@@ -1,5 +1,11 @@
 def add_number(a,b, c):
     return a + b + c
 
+
+# 1. extend add_numbers to allow an arbitrary number of parameters
+# 2. investigate and resolve security exploit of add_numbers()
+# 3. make the implementation of add_numbers() more performant
+# 4. add documentation for add_number()
+
 def calc_diff(a, b):
     return a - b
