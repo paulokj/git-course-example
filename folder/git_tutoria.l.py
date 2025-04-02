b2fcff04c606065a2622c0604f2570f9dd964db1
Paul Addai <pauljnraddai@yahoo.com>
2025-04-02 08:42:23 +0100

    Resolving code issue

--- folder/git_tutoria.l.py
+++ folder/git_tutoria.l.py
@@ -2,7 +2,10 @@ def add_number(a,b, c):
     """sumary_line
     
     Returns the sum of a,b and c
+
+     """ 
     return a + b + c
+
 
 
 # 1. extend add_numbers to allow an arbitrary number of parameters
